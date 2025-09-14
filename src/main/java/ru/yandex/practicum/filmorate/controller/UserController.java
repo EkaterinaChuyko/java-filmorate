7810b4fcaa564bcde13c7f5fc378c45fd0e07ebe
EkaterinaChuyko <kylefoxmay@yandex.ru>
2025-09-14 17:22:08 +0300

    Удалены неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -11,7 +10,6 @@ import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
